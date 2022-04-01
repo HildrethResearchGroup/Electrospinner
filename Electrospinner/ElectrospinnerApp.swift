b8c261dbb2261526a1e45bc5d2a9c3ef49219fad
Steven DiGregorio <sdigregorio@mines.edu>
2022-04-01 16:12:20 -0600

    Initial Commit

---
+++ Electrospinner/ElectrospinnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElectrospinnerApp.swift
+//  Electrospinner
+//
+//  Created by Steven DiGregorio on 4/1/22.
+//
+
+import SwiftUI
+
+@main
+struct ElectrospinnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
